846ec9fb13fc4ef12e53f98f0e48f97021c5c121
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2021-04-25 20:46:15 +0300

    refactoting code

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
+import gallery from '../gallery-items.js';
+
 const galleryListEl = document.querySelector(".js-gallery");
 const modal = document.querySelector('.lightbox');
 const modalImgEL = document.querySelector(".lightbox__image");
+const closeBtnEl = document.querySelector('[data-action="close-lightbox"]');
+const overlay = document.querySelector('.lightbox__overlay');
 
-//create markup
-import gallery from '../gallery-items.js';
-     
+closeBtnEl.addEventListener('click',onClickCLoseBtn);
+galleryListEl.addEventListener('click', onClickImgOpenModal);
+overlay.addEventListener('click', onOverlayClick);
+
 const galleryMarkupItem = gallery.reduce(
     (string, item) => string +
         `<li class="gallery__item">
@@ -16,55 +21,38 @@ const galleryMarkupItem = gallery.reduce(
   ""
 );
 
-//addes markup DOM
 galleryListEl.innerHTML = galleryMarkupItem;
 
-//Реализация делегирования на галерее ul.js-gallery 
-galleryListEl.addEventListener('click', onClickImgOpenModal);
-
-function onClickImgOpenModal(evt) {
-
-  window.addEventListener('keydown', onEscKeyPress);
-
-  evt.preventDefault();
-  if (evt.target.nodeName !== 'IMG') {
-    return;
-  }
-  //Получение url большого изображения
-  const urlBigImg = evt.target.getAttribute("data-source");
-
-  //Открытие модального окна по клику на элементе галереи.
-  modal.classList.add('is-open');
-
-  //Подмена значения атрибута src элемента img.lightbox__image
-  modalImgEL.setAttribute('src', urlBigImg);
-} 
-
-//Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]
-const closeBtnEl = document.querySelector('[data-action="close-lightbox"]');
-closeBtnEl.addEventListener('click',onClickCLoseBtn);
-
-function onClickCLoseBtn() {
-  modal.classList.remove('is-open');
-  //Очистка значения атрибута src элемента img.lightbox__image
-  modalImgEL.setAttribute('src', '');
+function onOverlayClick(evt) {
+    if (evt.currentTarget === evt.target) {
+      onClickCLoseBtn();
+    }
 }
-//Закрытие модального окна по нажатию клавиши ESC.
+
 function onEscKeyPress() {
-    const ESC_KEY_CODE = 'Escape';
-    const isEscKey = event.code === ESC_KEY_CODE;
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
 
   if (isEscKey) {
     onClickCLoseBtn();
   }
 }
-//Закрытие модального окна по клику на div.lightbox__overlay
-const overlay = document.querySelector('.lightbox__overlay');
-overlay.addEventListener('click', onOverlayClick);
 
-function onOverlayClick(evt) {
-    if (evt.currentTarget === evt.target) {
-    onClickCLoseBtn();
-  }
+function onClickCLoseBtn() {
+  modal.classList.remove('is-open');
+  modalImgEL.setAttribute('src', '');
 }
+     
+function onClickImgOpenModal(evt) {
+  evt.preventDefault();
 
+  window.addEventListener('keydown', onEscKeyPress);
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const urlBigImg = evt.target.getAttribute("data-source");
+  modal.classList.add('is-open');
+  modalImgEL.setAttribute('src', urlBigImg);
+}
