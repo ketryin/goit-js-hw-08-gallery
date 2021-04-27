9a6bf3e18f095eaedee15f591eed13ede4cc7c18
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2021-04-27 11:40:44 +0300

    addes scrolling through images

--- js/index.js
+++ js/index.js
@@ -21,6 +21,8 @@ const galleryMarkupItem = gallery.reduce(
   ""
 );
 
+const galleryUrlBigImg = gallery.map(element => element.original);
+
 galleryListEl.innerHTML = galleryMarkupItem;
 
 function onOverlayClick(evt) {
@@ -29,24 +31,32 @@ function onOverlayClick(evt) {
     }
 }
 
-function onEscKeyPress() {
+function onKeyPress(evt) {
   const ESC_KEY_CODE = 'Escape';
-  const isEscKey = event.code === ESC_KEY_CODE;
+  const LEFT_KEY_CODE = 'ArrowLeft';
+  const RIGHT_KEY_CODE = 'ArrowRight';
+  const isEscKey = evt.code === ESC_KEY_CODE;
+  const urlBigImg = modalImgEL.getAttribute("src");
+  const indexImg = galleryUrlBigImg.indexOf(urlBigImg);
 
   if (isEscKey) {
     onClickCLoseBtn();
+  } else if (evt.code===LEFT_KEY_CODE) {
+    onLeftKeyPress(indexImg)
+  } else if (evt.code===RIGHT_KEY_CODE) {
+    onRightKeyPress(indexImg)
   }
 }
 
 function onClickCLoseBtn() {
   modal.classList.remove('is-open');
-  modalImgEL.setAttribute('src', '');
+  modalImgEL.removeAttribute('src');
 }
      
 function onClickImgOpenModal(evt) {
   evt.preventDefault();
 
-  window.addEventListener('keydown', onEscKeyPress);
+  window.addEventListener('keydown', onKeyPress);
 
   if (evt.target.nodeName !== 'IMG') {
     return;
@@ -55,4 +65,18 @@ function onClickImgOpenModal(evt) {
   const urlBigImg = evt.target.getAttribute("data-source");
   modal.classList.add('is-open');
   modalImgEL.setAttribute('src', urlBigImg);
+  
+}
+
+function onLeftKeyPress(index) {
+  if (index === 0) {
+    return modalImgEL.setAttribute('src', galleryUrlBigImg[galleryUrlBigImg.length-1]);  
+  }
+  modalImgEL.setAttribute('src', galleryUrlBigImg[index-1]);
+}
+function onRightKeyPress(index) {
+  if (index === galleryUrlBigImg.length-1) {
+    return modalImgEL.setAttribute('src', galleryUrlBigImg[0]);  
+  }
+  modalImgEL.setAttribute('src', galleryUrlBigImg[index+1]);
 }
